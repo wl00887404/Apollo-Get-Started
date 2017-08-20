2c3840a0e43c2b4ac4bb21cb5a2f30bd01ac40a0
wl00887404 <wl00887404@gmail.com>
2017-08-20 14:05:38 +0800

    apollo => 查詢 Todos

--- src/Page/Todo/index.js
+++ src/Page/Todo/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { Hero, Tabs, InputHasAddons, Container, TodosTable } from '../../Component'
 
+import {
+    gql,
+    graphql
+} from 'react-apollo'
+
 let filter = [{
         id: "all",
         name: "全部"
@@ -34,6 +39,10 @@ class Page extends Component {
             name
         } = this.state
 
+        let {
+            todos
+        } = this.props.data
+
         return (
             <div>
                 <Hero title={"Lovely Todo List"} subtitle={"事情永遠做不完"}>
@@ -45,11 +54,25 @@ class Page extends Component {
                         <InputHasAddons
                             onChange={this.onChange.bind(this)}
                             value={name} />
-                        <TodosTable />
+                        <TodosTable 
+                            todos={todos} />
                 </Container>
             </div>
         )
     }
 }
 
-export default Page
+const qTodos = {
+    query: gql `
+        query {
+            todos{
+                id
+                name
+                createAt
+                finished
+            }
+        }
+    `
+}
+
+export default graphql(qTodos.query)(Page)
